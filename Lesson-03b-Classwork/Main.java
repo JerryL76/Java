187ab47871972af1ae4fd4bc824128bd6e50ad87
JerryL76 <jerryl76@nycstudents.net>
2025-09-11 13:30:46 +0000

    .

---
+++ Lesson-03b-Classwork/Main.java
@@ -0,0 +1,19 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+    System.out.println("Jerry Lin");
+    System.out.println("Jerry Lin \n full address");
+
+    
+   
+    
+  }
+
+  
+}
