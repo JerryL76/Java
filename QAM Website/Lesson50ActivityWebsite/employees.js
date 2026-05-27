572920b058ab7df50348a3b04641b8203b00dd50
JerryL76 <jerryl76@nycstudents.net>
2026-05-27 20:32:43 +0000

    .

--- QAM Website/Lesson50ActivityWebsite/employees.js
+++ QAM Website/Lesson50ActivityWebsite/employees.js
@@ -3,31 +3,25 @@ let employees;
 async function init() {
   let link = "https://turbo-space-cod-pjwjgwrx7ggp3rv56-8500.app.github.dev/employees";
 
-  try {
-    let response = await fetch(link);
-    employees = await response.json();
-    console.log("Data loaded:", employees);
-    displayEmployees(employees);
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
+  let info = await fetch(link);
+  employees = await info.json();
+
+  displayEmployees(employees);
 }
 
 function displayEmployees(data) {
   let output = "";
 
-
   for (let i = 1; i < data.length; i++) {
     output += `
       <div class="card">
         <h3>${data[i].field2}</h3>
-        <p><strong>Department:</strong> ${data[i].field3}</p>
+        <p><strong>Dept:</strong> ${data[i].field3}</p>
         <p><strong>Email:</strong> ${data[i].field4}</p>
         <p><strong>Location:</strong> ${data[i].field5}</p>
       </div>
     `;
   }
-
 
   document.getElementById("output").innerHTML = output;
 }
