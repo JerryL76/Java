bc90b1ec07c5ef5e84320350a8572c5a69c541fe
JerryL76 <jerryl76@nycstudents.net>
2026-05-27 20:04:33 +0000

    .

--- QAM Website/Lesson50ActivityWebsite/companies.js
+++ QAM Website/Lesson50ActivityWebsite/companies.js
@@ -15,14 +15,14 @@ function displayCompanies(data) {
   let output = "";
 
   for (let i = 0; i < data.length; i++) {
-    output += `
-      <div class="card">
-        <h3>${data[i].CompanyName}</h3>
-        <p>Industry: ${data[i].Industry}</p>
-        <p>Location: ${data[i].City}</p>
-      </div>
-    `;
-  }
+  output += `
+    <div class="card">
+      <h3>${data[i].company_name}</h3>
+      <p>Industry: ${data[i].industry}</p>
+      <p>Location: ${data[i].city}</p>
+    </div>
+  `;
+}
 
   document.getElementById("output").innerHTML = output;
 }
